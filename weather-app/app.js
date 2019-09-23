b58948e8eb72270ce579d6cc3e3549aa42055cc9
naren-mehta <deepakmehta.singh@gmail.com>
2019-09-23 23:34:59 +0530

    Working on weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,6 +7,7 @@ const url='https://api.darksky.net/forecast/0a303a4b92790b15e6eba6fa5fe62836/37.
 request({url: url, json:true},(error, response)=>{
 
     const currently= response.body.currently;
+    const daily= response.body.daily;
 
-    console.log('It is currently '+currently.temperature +' degree out. There is a '+ currently.precipProbability +'% chance of rain.');
+    console.log(daily.data[0].summary +' It is currently '+currently.temperature +' degree out. There is a '+ currently.precipProbability +'% chance of rain.');
 });
