fbacaf88b74069a91ff602e678eaaf6bd8e681b0
Narendra Singh Mehta <NarendraM@quinnox.com>
2019-09-24 17:31:47 +0530

    Working on ng

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,7 +4,7 @@ const request = require('request');
 const successChalk=chalk.green;
 const errorChalk=chalk.red;
 
-const weatherUrl = 'https://api.darksky.net/forecast/0a303a4b92790b15e6eba6fa5fe62836/37.8267,-122.4233?units=us';
+const weatherUrl = 'https://api.darksky.net/forecast/0a303a4b92790b15e6eba6fa5fe62836/37.8267,-122.4233';
 const geocodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/hongasandra%20bengaluru.json?access_token=pk.eyJ1IjoibmFyZW4xNyIsImEiOiJjazB4Nmo2ajcwMWM5M2huenZtd3E2bWZuIn0.-xTsU0tSZ6-FT9YV5MEpIQ&limit=1';
 
 
@@ -14,7 +14,6 @@ request({ url: weatherUrl, json: true }, (error, response) => {
         console.log(errorChalk('Unable to connect to weather service'));
     }else if(response.body.error){
         console.log(errorChalk('Unable to find location'));
-
     } else {
         const currently = response.body.currently;
         const daily = response.body.daily;
